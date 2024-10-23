da341e11ac4b789dff89bb09ea2238921ba5a5d9
Błażej Niewiadomski <blaz.nie@protonmail.com>
2024-10-23 09:36:24 +0200

    Add early-init

---
+++ early-init.el
@@ -0,0 +1,24 @@
+;;; early-init.el early-init emacs file -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2024 Błażej Niewiadomski
+;; Author: Błażej Niewiadomski <blaz.nie@protonmail.com>
+;; This file is not part of GNU Emacs.
+;;; Commentary:
+;;
+;;
+;;; Code:
+
+;; to increase lsp-mode performance
+(setenv "LSP_USE_PLISTS" "true")
+
+;; Don't flicker GUI elements on startup
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+;; We're using straight.el instead of package.el, no need to load it
+(setq package-enable-at-startup nil
+      evil-want-keybinding nil)
+
+
+;;; early-init.el ends here
