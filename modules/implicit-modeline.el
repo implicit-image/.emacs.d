67862d4d78ae0bf16e5a761781f1428f16a88339
Błażej Niewiadomski <blaz.nie@protonmail.com>
2024-10-24 18:12:51 +0200

    [modeline] remove telephone-line

--- modules/implicit-modeline.el
+++ modules/implicit-modeline.el
@@ -1,26 +1,5 @@
 
-
-(use-package telephone-line
-  :disabled
-  :demand
-  :init
-  (setq telephone-line-primary-right-separator 'telephone-line-nil
-	telephone-line-secondary-right-separator 'telephone-line-nil
-	telephone-line-primary-left-separator 'telephone-line-nil
-	telephone-line-secondary-left-separator 'telephone-line-nil)
-  (setq telephone-line-lhs
-	'((evil   . (telephone-line-evil-tag-segment))
-	  (nil .    (telephone-line-projectile-buffer-segment))
-	  (accent . (telephone-line-vc-segment
-		     telephone-line-process-segment))))
-  (setq telephone-line-rhs
-	'((accent . (telephone-line-airline-position-segment))
-	  (modal . (telephone-line-major-mode-segment))))
-  :config
-  (telephone-line-mode 1))
-
-
-;; display search candidate count on modeline
+;; display search/replace candidate count on modeline
 (use-package anzu
   :demand
   :after doom-modeline)
