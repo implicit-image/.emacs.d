e51a662207be4ac79d48fc8a55d6192eac06db34
Błażej Niewiadomski <blaz.nie@protonmail.com>
2024-11-19 14:40:45 +0100

    [treesitter] add evil-textobj-tree-sitter package

--- modules/implicit-treesitter.el
+++ modules/implicit-treesitter.el
@@ -1,5 +1,3 @@
-
-
 (use-package treesit-auto
   :demand
   :init
@@ -7,5 +5,8 @@
   :config
   (treesit-auto-add-to-auto-mode-alist 'all)
   (global-treesit-auto-mode))
+
+(use-package evil-textobj-tree-sitter
+  :after (treeesit evil))
 
 (provide 'implicit-treesitter)
