3f405cc478d2e439365de16238b9aa352fa79a18
Błażej Niewiadomski <blaz.nie@protonmail.com>
2024-11-19 14:27:18 +0100

    [checkers] add flycheck window config

--- modules/implicit-checkers.el
+++ modules/implicit-checkers.el
@@ -1,10 +1,18 @@
 
 (use-package flyspell)
 
-(use-package flycheck)
+(use-package flycheck
+  :init
+  (+windows/cfg '((flycheck-mode-major-mode)
+		  :position bottom :height 0.3 )))
 
-(use-package flycheck-inline
-  :after flycheck)
+(use-package sideline-flycheck
+  :after flycheck
+  :init
+  (setq sideline-backends-right '(sideline-flycheck))
+  :hook
+  (flycheck-mode . sideline-mode)
+  (flycheck-mode . sideline-flycheck-setup))
 
 (use-package flymake)
 
