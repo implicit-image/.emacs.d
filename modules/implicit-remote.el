764a7fb7ef36154ef661dad854b734acedfb4718
Błażej Niewiadomski <blaz.nie@protonmail.com>
2024-11-19 14:42:32 +0100

    [remote] add tramp package and commands

--- modules/implicit-remote.el
+++ modules/implicit-remote.el
@@ -1,8 +1,28 @@
 
+;;;###autoload
+(defun +remote/counsel-find-file ()
+  (interactive)
+  (ivy-read "Remote method:" (mapcar (lambda (method-form) (car method-form)) tramp-methods)
+	    :preselect 0
+	    :require-match t
+	    :history '+remote/counsel-find-file-history
+	    :action (lambda (method)
+		      (interactive)
+		      (counsel-find-file (string-join `("/" ,method "::"))))
+	    :caller '+remote/counsel-find-file))
+
+;;;###autoload
+(defun +remote/open-file-as-sudo ()
+  (interactive)
+  (counsel-find-file "/sudo::"))
+
+
 (use-package auth-source
   :demand
   :config
   (setq auth-sources '("~/.authinfo" "~/.authinfo.gpg" "~/.netrc")))
+
+(use-package tramp)
 
 
 
