1263d37891729eb5ea5d3e56aeb9b9622b7f0197
Błażej Niewiadomski <blaz.nie@protonmail.com>
2024-11-20 03:32:35 +0100

    [modules] update load-path setup

--- modules/implicit-modules.el
+++ modules/implicit-modules.el
@@ -2,13 +2,14 @@
 ;; TODO figure out what to do with this
 
 
-(defun my/load-submodule (modname submodname)
+(defun +modules/require (modname)
   (let ((mod-dir-path (concat my/modules-dir-path (symbol-name modname)))
 	(submod-path (concat mod-dir-path "/" (symbol-name submodname) ".el")))
+    (add-to-list 'load-path mod-dir-path)
     (require submodname submod-path)))
 
 (defun +modules/browse ()
   (interactive)
-  (counsel-find-file "" my/modules-dir-path))
+  (counsel-find-file "" +modules/path))
 
 (provide 'implicit-modules)
