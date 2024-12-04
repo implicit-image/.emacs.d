6d053cbee967736fb3c0c2cb5468e43e3a5bac48
Błażej Niewiadomski <blaz.nie@protonmail.com>
2024-12-05 00:13:46 +0100

    [checkers] add pretty errors picker

--- modules/init-checkers.el
+++ modules/init-checkers.el
@@ -1,4 +1,39 @@
+;;; -*- lexical-binding: t -*-
+
 (use-package flyspell)
+
+
+(defun +checkers--get-pretty-errors ()
+  "Get propertized errors from `flycheck'."
+  (require 'flycheck)
+  (mapcar
+   (lambda (err)
+     (let ((file-name (propertize (file-name-nondirectory (flycheck-error-filename err)) 'face 'flycheck-error-list-id))
+	   (line-number (propertize (number-to-string (flycheck-error-line err)) 'face 'flycheck-error-list-info))
+	   (error-message (propertize (flycheck-error-message err) 'face 'flycheck-error-list-checker-name)))
+       (concat file-name
+	       ":"
+	       line-number
+	       ":"
+	       error-message)))
+   flycheck-current-errors))
+
+;;;###autoload
+(defun +checkers/counsel-errors ()
+  ""
+  (interactive)
+  (require 'ivy)
+  (ivy-read "Errors: "
+	    (+checkers--get-pretty-errors)
+	    :preselect 1
+	    :require-match t
+	    :action (lambda (err)
+		      (flycheck-goto-line
+		       (string-to-number
+			(string-split err ":"))))
+	    :caller '+checkers/counsel-errors))
+
+
 
 (use-package flycheck
   :init
@@ -11,7 +46,7 @@
    :states '(normal visual)
    :prefix "SPC"
    :global-prefix "M-SPC"
-   "s e" '("Flycheck errors" . counsel-flycheck))
+   "s e" '("Flycheck errors" . +checkers/counsel-errors))
   (flycheck-mode-map
    :states '(normal visual insert)
    "C-c ]" 'flycheck-next-error
