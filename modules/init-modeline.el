e5e62f4e6d81d8bbc1a24c6fca9e01a77934e1c2
Błażej Niewiadomski <blaz.nie@protonmail.com>
2024-12-05 00:22:37 +0100

    [modeline] change mood-line format

--- modules/init-modeline.el
+++ modules/init-modeline.el
@@ -16,7 +16,9 @@
 
 (use-package mood-line
   :custom-face
-  (mode-line ((t (:background ,(doom-color 'bg-alt)))))
+  (mode-line ((t (:background ,(doom-color 'base4)))))
+  (mode-line-inactive ((t (:background ,(doom-color 'base3)))))
+  (mode-line-active ((t (:background ,(doom-color 'base4)))))
   :demand
   ;; Enable mood-line
   :config
@@ -24,15 +26,20 @@
 	(mood-line-defformat
 	 :left
 	 (((mood-line-segment-modal) . " ")
-	  ((mood-line-segment-project) . "")
+	  ((mood-line-segment-anzu) . " ")
+	  ((propertize (mood-line-segment-project)
+		       'face 'mood-line-status-success)
+	   . "/")
 	  ((mood-line-segment-buffer-status) . ":")
 	  ((mood-line-segment-buffer-name) . " ")
 	  ((mood-line-segment-cursor-position) . " ")
 	  ((mood-line-segment-multiple-cursors) . ""))
 	 :right
-	 (((mood-line-segment-checker) . " ")
-	  ((mood-line-segment-major-mode) . " ")
-	  ((mood-line-segment-vc) . ""))))
+	 (((mood-line-segment-misc-info) . " ")
+	  ((mood-line-segment-checker) . " ")
+	  ((mood-line-segment-vc) . " ")
+	  ((mood-line-segment-misc-info) . " ")
+	  ((mood-line-segment-major-mode) . " "))))
   (mood-line-mode))
 
 (provide 'init-modeline)
