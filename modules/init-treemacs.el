7264989d2e26898492f2e3894fddb3e2d3ec47b2
Błażej Niewiadomski <blaz.nie@protonmail.com>
2024-12-05 01:09:01 +0100

    [treemacs] remove icons

--- modules/init-treemacs.el
+++ modules/init-treemacs.el
@@ -1,8 +1,10 @@
+;;; -*- lexical-binding: t -*-
+
 (use-package treemacs
-  :demand
   :init
   (+windows-cfg '((treemacs-mode) :position left :width 0.18 :noselect t :stick t :dedicated t))
   (setq treemacs-indent-guide-style 'line
+	treemacs-eldoc-display 'detailed
 	treemacs-indentation 1)
   :config
   (treemacs-load-theme "Default")
@@ -26,7 +28,5 @@
 (use-package treemacs-magit
   :demand
   :after (treemacs magit))
-
-(use-package treemacs-all-the-icons)
 
 (provide 'init-treemacs)
