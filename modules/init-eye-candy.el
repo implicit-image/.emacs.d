a41e996a9ae6ed59b5e2c6d05914e02021c608e2
Błażej Niewiadomski <blaz.nie@protonmail.com>
2024-12-09 23:37:12 +0100

    [eye-candy] fix colors

--- modules/init-eye-candy.el
+++ modules/init-eye-candy.el
@@ -1,46 +1,13 @@
+;;; -*- lexical-binding: t -*-
+
 (require 'f)
 
 (defun +eye-candy/load-theme (theme)
   ""
   (interactive)
+  (require 'solaire-mode)
   (load-theme theme t)
   (solaire-global-mode +1))
-
-(use-package all-the-icons
-  :if (display-graphic-p))
-
-(use-package rainbow-delimiters
-  :hook ((prog-mode emacs-lisp-mode) . rainbow-delimiters-mode))
-
-(use-package rainbow-mode
-  :hook
-  (css-mode . rainbow-mode)
-  (css-ts-mode . rainbow-mode)
-  (helpful-mode . rainbow-mode))
-
-(use-package hl-todo
-  :after doom-themes
-  :config
-  (setq hl-todo-keyword-faces
-	`(("HOLD" . "#d0bf8f")
-	  ("TODO" . "#cc9393")
-	  ("NEXT" . "#dca3a3")
-	  ("THEM" . "#dc8cc3")
-	  ("PROG" . "#7cb8bb")
-	  ("OKAY" . "#7cb8bb")
-	  ("DONT" . "#5f7f5f")
-	  ("FAIL" . "#8c5353")
-	  ("DONE" . "#afd8af")
-	  ("NOTE" . "#d0bf8f")
-	  ("MAYBE" . "#d0bf8f")
-	  ("KLUDGE" . "#d0bf8f")
-	  ("HACK" . "#d0bf8f")
-	  ("TEMP" . "#d0bf8f")
-	  ("FIXME" . "#cc9393")
-	  ("XXXX*" . "#cc9393")))
-  (global-hl-todo-mode 1))
-
-(use-package olivetti)
 
 (use-package solaire-mode
   :after doom-themes)
@@ -65,6 +32,51 @@
   (tty-setup . (lambda ()
 		 (interactive)
 		 (+eye-candy/load-theme +base/theme))))
+
+(use-package all-the-icons
+  :if (display-graphic-p))
+
+(use-package nerd-icons
+  :general
+  (+leader-keys
+    "i i" '("Insert unicode icon" . nerd-icons-insert)))
+
+(use-package rainbow-delimiters
+  :custom-face
+  (rainbow-delimiters-depth-8-face ((t (:foreground ,(doom-color 'teal)))))
+  :hook ((prog-mode emacs-lisp-mode) . rainbow-delimiters-mode))
+
+(use-package rainbow-mode
+  :hook
+  (css-mode . rainbow-mode)
+  (css-ts-mode . rainbow-mode)
+  (helpful-mode . rainbow-mode))
+
+(use-package hl-todo
+  :commands
+  global-hl-todo-mode
+  :init
+  (setq hl-todo-keyword-faces
+	`(("HOLD" . "#d0bf8f")
+	  ("TODO" . "#cc9393")
+	  ("NEXT" . "#dca3a3")
+	  ("THEM" . "#dc8cc3")
+	  ("PROG" . "#7cb8bb")
+	  ("OKAY" . "#7cb8bb")
+	  ("DONT" . "#5f7f5f")
+	  ("FAIL" . "#8c5353")
+	  ("DONE" . "#afd8af")
+	  ("NOTE" . "#d0bf8f")
+	  ("MAYBE" . "#d0bf8f")
+	  ("KLUDGE" . "#d0bf8f")
+	  ("HACK" . "#d0bf8f")
+	  ("TEMP" . "#d0bf8f")
+	  ("FIXME" . "#cc9393")
+	  ("XXXX*" . "#cc9393")))
+  :hook
+  (after-init . global-hl-todo-mode))
+
+(use-package olivetti)
 
 (use-package doom-gruber-darker-theme
   :straight (doom-gruber-darker-theme :type git
