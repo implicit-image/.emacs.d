1f2c2293306c5343217c57e34f1dca470b72317c
Błażej Niewiadomski <blaz.nie@protonmail.com>
2025-05-08 00:59:33 +0000

    fix up base

--- modules/init-base.el
+++ modules/init-base.el
@@ -3,16 +3,35 @@
 (use-package emacs
   :demand
   :init
-  (require 'f)
+  (defun +insert-scratch-buffer-info ()
+    (interactive)
+    (with-current-buffer (get-buffer-create "*scratch*")
+      (insert (format ";;
+;; ███████╗███╗   ███╗ █████╗  ██████╗███████╗
+;; ██╔════╝████╗ ████║██╔══██╗██╔════╝██╔════╝
+;; █████╗  ██╔████╔██║███████║██║     ███████╗
+;; ██╔══╝  ██║╚██╔╝██║██╔══██║██║     ╚════██║
+;; ███████╗██║ ╚═╝ ██║██║  ██║╚██████╗███████║
+;; ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝╚══════╝
+;;
+;;   Loading time : %s
+;;   Packages     : %s
+;;
+"
+                      (emacs-init-time)
+                      (number-to-string (hash-table-count straight--recipe-cache)))))
+
+    (message (emacs-init-time))
+    (lisp-interaction-mode))
 
   (defun +set-global-font-size ()
     (interactive)
     (let* ((weight +base/font-weight)
-          (family +base/font-family)
-          (size (read-number "Font size: "))
-          (spec (font-spec :family family
-                           :weight weight
-                           :size size)))
+           (family +base/font-family)
+           (size (read-number "Font size: "))
+           (spec (font-spec :family family
+                            :weight weight
+                            :size size)))
       (set-frame-font spec nil t t)))
   ;;;; Custom global options
   (setq +base/font-family "Comic Code Ligatures"
@@ -45,20 +64,39 @@
         truncate-partial-width-windows t
         ;;tempfiles
         create-lockfiles nil
+        backup-inhibited t
+        make-backup-files nil
         ;; put backups in a shared directory
-        backup-directory-alist `(("." . ,(f-join user-emacs-directory "backups")))
+        backup-directory-alist `(("." . ,(expand-file-name "backups" user-emacs-directory)))
         ;; increase garbage collector limit
         ;; this increases the time emacs can run w/o running garbage collector,
         ;; but also increases time the garbage collection takes when it is needed
         ;; 10 MB is a nice in-between value
-        gc-cons-threshold (* 1024 1024 10)
         ;; enable recursive minibuffers for vertico
         enable-recursive-minibuffers t
         ;; scrollling
         scroll-step 1
         scroll-margin 15
         ;; TODO: find useful wildard settings
-        find-file-wildcards nil)
+        find-file-wildcards nil
+        ;; comments
+        comment-multi-line t
+        comment-empty-lines t
+        lazy-highlight-initial-delay 0
+        ;; completion
+        completion-ignore-case t
+        use-dialog-box nil
+        use-file-dialog nil
+        use-short-answers t
+        read-answer-short t)
+
+  (setq-default left-fringe-width 5
+                right-fringe-width 5
+                indicate-buffer-boundaries nil
+                indicate-empty-lines nil
+                word-wrap t
+                indent-tabs-mode nil
+                tab-width 4)
   :config
   ;; always use short user input prompts
   (defalias 'yes-or-no-p 'y-or-n-p)
@@ -75,6 +113,8 @@
   (global-hl-line-mode 1)
   ;; show borders between emacs windows
   (window-divider-mode 1)
+  ;; save window layout changes
+  (winner-mode 1)
   ;; dont auto-revert buffers
   (global-auto-revert-mode nil)
   ;; dont show scroll-bars
@@ -93,7 +133,6 @@
   ;; better to see all of the code
   (prog-mode-hook . (lambda ()
                       (interactive)
-                      (toggle-truncate-lines 1)
                       (visual-wrap-prefix-mode 1)))
   ;; display line numbers in text-editing modes
   ((prog-mode-hook
@@ -107,9 +146,11 @@
     tuareg-mode-hook)
    . display-line-numbers-mode)
   (window-setup-hook . toggle-frame-fullscreen)
+  (window-setup-hook . +insert-scratch-buffer-info)
   :general
   (+leader-keys
-    "t f" '+set-global-font-size))
+    "t f" '+set-global-font-size
+    "t I" '("Select input method." . set-input-method)))
 
 ;; load $PATH from shell
 (use-package exec-path-from-shell
@@ -123,12 +164,6 @@
                                               ((+os/is-windows-p) "powershell")))
   :hook
   (after-init . exec-path-from-shell-initialize))
-
-(use-package mule
-  :straight nil
-  :general
-  (+leader-keys
-    "t I" '("Select input method" . set-input-method)))
 
 (use-package restart-emacs
   :general
